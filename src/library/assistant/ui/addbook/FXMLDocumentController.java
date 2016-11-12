9871db4860248486c2d483aece30a73ccb196818
Muhammed Afsal Villan <afsalashyana@gmail.com>
2016-11-12 14:52:49 +0530

    End of Fourth tutorial

--- src/library/assistant/ui/addbook/FXMLDocumentController.java
+++ src/library/assistant/ui/addbook/FXMLDocumentController.java
@@ -3,11 +3,17 @@ package library.assistant.ui.addbook;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
 import java.net.URL;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 import library.assistant.database.DatabaseHandler;
 
 public class FXMLDocumentController implements Initializable {
@@ -26,10 +32,14 @@ public class FXMLDocumentController implements Initializable {
     private JFXButton cancelButton;
 
     DatabaseHandler databaseHandler;
+    @FXML
+    private AnchorPane rootPane;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         databaseHandler = new DatabaseHandler();
+        
+        checkData();
     }
 
     @FXML
@@ -47,13 +57,6 @@ public class FXMLDocumentController implements Initializable {
             return;
         }
 
-//        stmt.execute("CREATE TABLE " + TABLE_NAME + "("
-//                        + "	id varchar(200) primary key,\n"
-//                        + "	title varchar(200),\n"
-//                        + "	author varchar(200),\n"
-//                        + "	publisher varchar(100),\n"
-//                        + "	isAvail boolean default true"
-//                        + " )");
         String qu = "INSERT INTO BOOK VALUES ("
                 + "'" + bookID + "',"
                 + "'" + bookName + "',"
@@ -78,6 +81,21 @@ public class FXMLDocumentController implements Initializable {
 
     @FXML
     private void cancel(ActionEvent event) {
+        Stage stage = (Stage) rootPane.getScene().getWindow();
+        stage.close();
+    }
+
+    private void checkData() {
+        String qu = "SELECT title FROM BOOK";
+        ResultSet rs = databaseHandler.execQuery(qu);
+        try {
+            while(rs.next()){
+                String titlex = rs.getString("title");
+                System.out.println(titlex);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
 }
