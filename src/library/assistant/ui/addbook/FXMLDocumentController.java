acb69510cb8d956e1596569c85464adbecf3dc59
Muhammed Afsal Villan <afsalashyana@gmail.com>
2016-11-12 12:19:26 +0530

    End of first tutorial

---
+++ src/library/assistant/ui/addbook/FXMLDocumentController.java
@@ -0,0 +1,45 @@
+package library.assistant.ui.addbook;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+/**
+ *
+ * @author villan
+ */
+public class FXMLDocumentController implements Initializable {
+
+    @FXML
+    private JFXTextField title;
+    @FXML
+    private JFXTextField id;
+    @FXML
+    private JFXTextField author;
+    @FXML
+    private JFXTextField publisher;
+    @FXML
+    private JFXButton saveButton;
+    @FXML
+    private JFXButton cancelButton;
+    
+    
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void addBook(ActionEvent event) {
+    }
+
+    @FXML
+    private void cancel(ActionEvent event) {
+    }
+    
+}
---
+++ src/library/assistant/ui/addbook/LibraryAssistant.java
@@ -0,0 +1,29 @@
+package library.assistant.ui.addbook;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class LibraryAssistant extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
