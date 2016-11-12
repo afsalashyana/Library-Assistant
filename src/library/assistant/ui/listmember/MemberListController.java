db1e956b270ed343677a63465a7d346a03a8fb7c
Muhammed Afsal Villan <afsalashyana@gmail.com>
2016-11-12 18:20:26 +0530

    COMMIT-1

---
+++ src/library/assistant/ui/listmember/MemberListController.java
@@ -0,0 +1,103 @@
+package library.assistant.ui.listmember;
+
+import java.net.URL;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import library.assistant.database.DatabaseHandler;
+import library.assistant.ui.addbook.BookAddController;
+import library.assistant.ui.listbook.BookListController;
+
+public class MemberListController implements Initializable {
+
+    ObservableList<Member> list = FXCollections.observableArrayList();
+
+    @FXML
+    private TableView<Member> tableView;
+    @FXML
+    private TableColumn<Member, String> nameCol;
+    @FXML
+    private TableColumn<Member, String> idCol;
+    @FXML
+    private TableColumn<Member, String> mobileCol;
+    @FXML
+    private TableColumn<Member, String> emailCol;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        initCol();
+        loadData();
+    }
+
+    private void initCol() {
+        nameCol.setCellValueFactory(new PropertyValueFactory<>("name"));
+        idCol.setCellValueFactory(new PropertyValueFactory<>("id"));
+        mobileCol.setCellValueFactory(new PropertyValueFactory<>("mobile"));
+        emailCol.setCellValueFactory(new PropertyValueFactory<>("email"));
+    }
+
+    private void loadData() {
+        DatabaseHandler handler = new DatabaseHandler();
+        String qu = "SELECT * FROM MEMBER";
+        ResultSet rs = handler.execQuery(qu);
+        try {
+            while (rs.next()) {
+                String titlex = rs.getString("title");
+                String author = rs.getString("title");
+                String id = rs.getString("title");
+                String publisher = rs.getString("title");
+                Boolean avail = rs.getBoolean("isAvail");
+
+                BookListController.Book book = new BookListController.Book(titlex, id, author, publisher, avail);
+                list.add(book);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(BookAddController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        tableView.getItems().setAll(list);
+    }
+
+    public static class Member {
+
+        private final SimpleStringProperty name;
+        private final SimpleStringProperty id;
+        private final SimpleStringProperty mobile;
+        private final SimpleStringProperty email;
+
+        public Member(String name, String id, String mobile, String email) {
+            this.name = new SimpleStringProperty(name);
+            this.id = new SimpleStringProperty(id);
+            this.mobile = new SimpleStringProperty(mobile);
+            this.email = new SimpleStringProperty(email);
+        }
+
+        public String getName() {
+            return name.get();
+        }
+
+        public String getId() {
+            return id.get();
+        }
+
+        public String getMobile() {
+            return mobile.get();
+        }
+
+        public String getEmail() {
+            return email.get();
+        }
+
+    }
+
+}
