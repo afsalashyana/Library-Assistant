3991f87782ed5a45c9e0feb391df68cf2521d017
Muhammed Afsal Villan <afsalashyana@gmail.com>
2016-11-14 15:51:37 +0530

    End of 12th Tutorial

--- src/library/assistant/ui/main/MainController.java
+++ src/library/assistant/ui/main/MainController.java
@@ -1,7 +1,10 @@
 package library.assistant.ui.main;
 
+import com.jfoenix.effects.JFXDepthManager;
 import java.io.IOException;
 import java.net.URL;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -11,14 +14,42 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import library.assistant.database.DatabaseHandler;
 
 public class MainController implements Initializable {
 
+    @FXML
+    private HBox book_info;
+    @FXML
+    private HBox member_info;
+    @FXML
+    private TextField bookIDInput;
+    @FXML
+    private Text bookName;
+    @FXML
+    private Text bookAuthor;
+    @FXML
+    private Text bookStatus;
+    
+    DatabaseHandler databaseHandler;
+    @FXML
+    private TextField memberIDInput;
+    @FXML
+    private Text memberName;
+    @FXML
+    private Text memberMobile;
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        JFXDepthManager.setDepth(book_info, 1);
+        JFXDepthManager.setDepth(member_info, 1);
+        
+        databaseHandler = DatabaseHandler.getInstance();
     }
 
     @FXML
@@ -54,4 +85,78 @@ public class MainController implements Initializable {
         }
     }
 
+    @FXML
+    private void loadBookInfo(ActionEvent event) {
+        clearBookCache();
+        
+        String id  = bookIDInput.getText();
+        String qu = "SELECT * FROM BOOK WHERE id = '" + id + "'";
+        ResultSet rs = databaseHandler.execQuery(qu);
+        Boolean flag = false;
+        try {
+            while(rs.next())
+            {
+                String bName = rs.getString("title");
+                String bAuthor = rs.getString("author");
+                Boolean bStatus = rs.getBoolean("isAvail");
+                
+                bookName.setText(bName);
+                bookAuthor.setText(bAuthor);
+                String status = (bStatus)?"Available" : "Not Available";
+                bookStatus.setText(status);
+                
+                flag = true;
+            }
+            
+            if(!flag){
+                bookName.setText("No Such Book Available");
+            }
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(MainController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    void clearBookCache()
+    {
+        bookName.setText("");
+        bookAuthor.setText("");
+        bookStatus.setText("");
+    }
+    
+    void clearMemberCache()
+    {
+        memberName.setText("");
+        memberMobile.setText("");
+    }
+    
+
+    @FXML
+    private void loadMemberInfo(ActionEvent event) {
+        clearMemberCache();
+        
+        String id  = memberIDInput.getText();
+        String qu = "SELECT * FROM MEMBER WHERE id = '" + id + "'";
+        ResultSet rs = databaseHandler.execQuery(qu);
+        Boolean flag = false;
+        try {
+            while(rs.next())
+            {
+                String mName = rs.getString("name");
+                String mMobile = rs.getString("mobile");
+                
+                memberName.setText(mName);
+                memberMobile.setText(mMobile);
+                
+                flag = true;
+            }
+            
+            if(!flag){
+                memberName.setText("No Such Member Available");
+            }
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(MainController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
